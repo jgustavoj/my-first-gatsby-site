2316d1d28611cde7b4ea9481ed6639bc334021fd
Gus Jimenez <jimenezgustavoandres@gmail.com>
2023-04-13 12:05:55 -0400

    static images and plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 
 const IndexPage = () => {
@@ -8,6 +9,9 @@ const IndexPage = () => {
     <Layout pageTitle={"Home Page"}>
       <Link to="/about">About</Link>
       <p>I'm making this by following Gatsby Tutorial.</p>
+      <StaticImage
+      alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at camera"
+      src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large" />
     </Layout>
   )
 }
