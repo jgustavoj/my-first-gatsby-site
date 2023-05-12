59b610fae0310f8f34c293fc0ec0136555159af5
Gus Jimenez <jimenezgustavoandres@gmail.com>
2023-05-12 14:29:50 -0400

    update image

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,7 @@ const Aboutpage = () => {
         <Layout pageTitle={"About Me"}>
             <Link to="/">Back to Home</Link>
             <p>Hi there! I'm the proud creator of this site, which i built with Gatsby.js.</p>
+            <p>This is another line </p>
         </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,8 @@ const IndexPage = () => {
       <Link to="/about">About</Link>
       <p>I'm making this by following Gatsby Tutorial.</p>
       <StaticImage
-      alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at camera"
-      src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large" />
+      alt="Chef talking to custsomer in kitchen"
+      src="../../src/images/chef.jpg" />
     </Layout>
   )
 }
